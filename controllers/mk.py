25e69546d7e31739ddb121702e3aa10d054758ea
itays123 <itays2005@gmail.com>
2023-07-28 19:49:21 +0300

    Continue mk routes

--- controllers/mk.py
+++ controllers/mk.py
@@ -1,4 +1,6 @@
-from flask import abort
+from flask import abort, request
+
+from models.kneset_member import KnesetMember
 from .. import app, mkService
 
 @app.get('/api/mk')
@@ -11,3 +13,33 @@ def get_mk_by_id(id):
     if (mk == None):
         abort(404)
     return mk
+
+@app.get('/api/search')
+def search_mks():
+    query = request.args.get('query')
+    if (query == None):
+        return []
+    return mkService.search_by_name(query)
+
+def getDataFromRequest():
+    data = request.json
+    name = data.get("name")
+    partyName = data.get("partyName")
+    personalInfo = data.get("personalInfo")
+    if None in (name, partyName, personalInfo):
+        abort(422)
+    return KnesetMember(name, partyName, personalInfo)
+
+@app.post('/api/mk')
+def insert_mk():
+    mk = getDataFromRequest()
+    return mkService.insert(mk)
+
+@app.patch('/api/mk/<id>')
+def update_mk(id):
+    mk = getDataFromRequest()
+    return mkService.update(id, mk)
+
+@app.delete('/api/mk/<id>')
+def delete_mk(id):
+    return mkService.delete(id)
