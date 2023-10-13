8a6e783694f4b642042803600ebbc59eecf65171
itays123 <itays2005@gmail.com>
2023-10-13 19:12:13 +0300

    INIT: Abstract token repository

---
+++ repository/token_repository.py
@@ -0,0 +1,9 @@
+
+
+class TokenRepository:
+
+    def tokenize(self, username) -> str:
+        raise "Method not Implemented"
+    
+    def validate(self, jwt) -> str: 
+        raise "Method not implemented"
