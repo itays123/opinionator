f5c2f6f459cc18f70cb3191b2fb333093954d0f7
itays123 <itays2005@gmail.com>
2023-10-14 16:48:39 +0300

    FIX: Python jwt hashing

--- repository/jwt/jwt_repository.py
+++ repository/jwt/jwt_repository.py
@@ -2,14 +2,16 @@ from jwt import encode, decode, InvalidSignatureError
 from repository.token_repository import TokenRepository
 from config.env import jwt_secret
 
+algorithm = "HS256"
+
 class JWTRepository(TokenRepository):
 
     def tokenize(self, username) -> str:
-        return encode({ "user": username }, jwt_secret)
+        return encode({ "user": username }, jwt_secret, algorithm=algorithm)
     
     def validate(self, jwt) -> str | None:
         try:
-            payload = decode(jwt, jwt_secret)
+            payload = decode(jwt, jwt_secret, algorithms=[algorithm])
             return payload["user"]
         except InvalidSignatureError:
             return None
